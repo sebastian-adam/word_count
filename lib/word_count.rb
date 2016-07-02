24b7c1f97232aed005be6bb74cb94f8f552ac078
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-07-01 19:31:56 -0700

    update method to pass third rspec test

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,6 +1,6 @@
 class String
   def word_count(target_word)
-    if target_word == self
+    if target_word.upcase == self.upcase
       1
     else
       0
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -10,10 +10,10 @@ describe('String#word_count') do
   it("takes two words from the user and returns a fixed number of 1 if the words are identical") do
     expect("word".word_count("word")).to(eq(1))
   end
-#
-#   it("takes two words from the user and returns one iteration of the word if the words are alike, ignoring capitalization") do
-#     expect("Word".word_count("word")).to(eq("word"))
-#   end
+
+  it("takes two words from the user and returns a fixed number of 1 if the words are alike, ignoring capitalization") do
+    expect("Word".word_count("word")).to(eq(1))
+  end
 #
 #   it("takes one word and one sentence from the user and returns one iteration each alike word, ignoring capitalization") do
 #     expect("This is a sEnTeNcE".word_count("sentence")).to(eq("sentence"))
